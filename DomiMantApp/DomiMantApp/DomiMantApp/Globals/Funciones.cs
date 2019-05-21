39df3c36a49d5a566e1955d81dab32617e6bfdea
GRUPORIZEK\jose.moreno <jmoreno@rizekcacao.com>
2019-05-21 12:27:14 -0400

    Guardando clases globales

---
+++ DomiMantApp/DomiMantApp/DomiMantApp/Globals/Funciones.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace DomiMantApp.Globals
+{
+    using static System.Environment;    
+    public class Funciones
+    {
+        public static string GetDbPath() {
+            string folder = GetFolderPath(SpecialFolder.Personal);
+            string rutaDb = System.IO.Path.Combine(folder, "DomiMantApps.db");
+
+            return rutaDb;
+        }
+    }
+}
---
+++ DomiMantApp/DomiMantApp/DomiMantApp/Globals/Variables.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace DomiMantApp.Globals
+{    
+    public class Variables
+    {
+        public enum TipoUsuario {
+            Cliente=1,
+            Suplidor=2
+        }
+    }
+}
